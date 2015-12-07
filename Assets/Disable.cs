6f0fe49128c8fbcb99507a0c55caa7d752ad40cb
tommtom9 <tommtom9@live.nl>
2015-12-07 13:30:33 +0100

    Added project so far
    I made a simple test project that plays around with vision

---
+++ Assets/Disable.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Disable : MonoBehaviour {
+
+    Light light;
+	// Use this for initialization
+	void Start () {
+        light = GetComponent<Light>();
+        light.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour {
+
+    CharacterController controller;
+    public float speed = 3.0F;
+    public float rotateSpeed = 3.0F;
+    public float visionrangeRange = 3;
+    public float maxVisionRange = 15;
+    private Light lighting;
+
+    private float vector3Add;
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        lighting = GetComponentInChildren<Light>();
+    }
+    void Update()
+    {
+
+        transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        float curSpeed = speed * Input.GetAxis("Vertical");
+        controller.SimpleMove(forward * curSpeed);
+
+        vector3Add = controller.velocity.magnitude;
+
+        Debug.Log(controller.velocity.magnitude * visionrangeRange);
+        lighting.range = maxVisionRange - (Mathf.Abs(vector3Add) * visionrangeRange);
+    }
+
+}
