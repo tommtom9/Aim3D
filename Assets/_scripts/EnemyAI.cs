77a71da1d07b3d72f9fa99e5aedef544b7e8060c
tommtom9 <tommtom9@live.nl>
2015-12-07 16:35:32 +0100

    Added basic AI

---
+++ Assets/_scripts/EnemyAI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour {
+
+    GameObject player;
+    NavMeshAgent navMashA;
+	// Use this for initialization
+	void Start () {
+        navMashA = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        navMashA.SetDestination(player.transform.position);
+	}
+}
